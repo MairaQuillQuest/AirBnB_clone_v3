5d8e40ea06c8aec62f6aa63dec10bad79473118e
MairaQuillQuest <ririsoniah@gmail.com>
2024-05-03 09:12:37 -0700

    [Feature]: Adding AirBnB clone - RESTful API project

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,28 +4,26 @@ Module index:
    url_prefix = '/api/v1'
    routes:
        - '/status'
+       - '/stats'
 """
-from api.v1.views import app_views
+
 from flask import jsonify
 from models import storage
-
+from api.v1.views import app_views
 
 @app_views.route('/status')
 def status():
-    """ Method status """
+    """Return the status of the API"""
     return jsonify({"status": "OK"})
-
 
 @app_views.route('/stats')
 def stats():
-    """ Method stast """
-    return jsonify(
-        {
-            "amenities": storage.count("Amenity"),
-            "cities": storage.count("City"),
-            "places": storage.count("Place"),
-            "reviews": storage.count("Review"),
-            "states": storage.count("State"),
-            "users": storage.count("User")
-        }
-    )
+    """Retrieve the number of each object by type"""
+    return jsonify({
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    })
